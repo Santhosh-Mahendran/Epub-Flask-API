179cc88ca152c3a81fd018a77285caf26d1543a4
Santhosh-Mahendran <108713604+Santhosh-Mahendran@users.noreply.github.com>
2025-03-19 01:12:04 +0530

    Add files via upload

--- app/config.py
+++ app/config.py
@@ -9,19 +9,20 @@ class Config:
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = 7 * 24 * 60 * 60  # 7 days
 
-    # File Upload Configuration
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Get base directory
-    FILE_UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads/files')# Save uploads in 'uploads' folder
-    IMAGE_UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads/cover_images')
+    # Determine storage path
+    if os.getenv('VERCEL'):
+        FILE_UPLOAD_FOLDER = '/tmp/uploads/files'
+        IMAGE_UPLOAD_FOLDER = '/tmp/uploads/cover_images'
+    else:
+        BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+        FILE_UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads/files')
+        IMAGE_UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads/cover_images')
+
     ALLOWED_EXTENSIONS = {'pdf', 'epub', 'jpg', 'jpeg', 'png'}  # Allowed file types
 
     # Encryption Key
     ENCRYPTION_KEY = os.getenv('FILE_ENCRYPTION_KEY').encode()
 
-    # Ensure the upload folder exists
-    if not os.path.exists(FILE_UPLOAD_FOLDER):
-        os.makedirs(FILE_UPLOAD_FOLDER)
-
-    # Ensure the upload folder exists for cover image
-    if not os.path.exists(IMAGE_UPLOAD_FOLDER):
-        os.makedirs(IMAGE_UPLOAD_FOLDER)
+    # Ensure the upload folders exist
+    os.makedirs(FILE_UPLOAD_FOLDER, exist_ok=True)
+    os.makedirs(IMAGE_UPLOAD_FOLDER, exist_ok=True)
